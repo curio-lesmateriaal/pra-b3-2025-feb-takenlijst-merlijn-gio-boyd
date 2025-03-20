7eb8b839296b16ef7972dc77da4cc6fa2f2f047e
iAmM_777 <m02403992@gmail.com>
2025-03-20 10:06:33 +0100

    (fix) 1.2 Overzicht van openstaande taken
    query was fout + doing & done was nog niet klaar. is nu gefixt.

--- boord.php
+++ boord.php
@@ -50,11 +50,41 @@
 
             <div class="colum">
                 <h2 style="border-bottom: 3px solid greenyellow;">Doing</h2>
+                <?php
+                $query = "SELECT * FROM taken WHERE afdeling = :afdeling AND status = 'doing'";
+                $statement = $conn->prepare($query);
+                $statement ->execute([":afdeling" => $afdeling]);
+                $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+
+                <?php foreach ($meldingen as $melding): ?>
+                    <div class="card" id="<?php echo $melding['id'] ?>">
+                        <div class="top" style="background-color: greenyellow;"></div>
+                        <h3><?php echo $melding['id'] . ' - ' . $melding['titel']; ?></h3>
+                        
+                        
+                    </div>   
+                <?php endforeach; ?>
 
             </div>
 
             <div class="colum">
                 <h2 style="border-bottom: 3px solid greenyellow;">Done</h2>
+                <?php
+                $query = "SELECT * FROM taken WHERE afdeling = :afdeling AND status = 'done'";
+                $statement = $conn->prepare($query);
+                $statement ->execute([":afdeling" => $afdeling]);
+                $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+
+                <?php foreach ($meldingen as $melding): ?>
+                    <div class="card" id="<?php echo $melding['id'] ?>">
+                        <div class="top" style="background-color: greenyellow;"></div>
+                        <h3><?php echo $melding['id'] . ' - ' . $melding['titel']; ?></h3>
+                        
+                        
+                    </div>   
+                <?php endforeach; ?>
 
             </div>
             
